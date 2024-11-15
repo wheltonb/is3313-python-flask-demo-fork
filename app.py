b5f159cc363c5019abceb398f2e886b1d7bac4c2
simonwoodworth <s.woodworth@ucc.ie>
2024-11-15 19:17:00 +0000

    Force port to 80

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def hello_world():  # put application's code here
     return f'Hello World! The answer is: {powerString}'
 
 if __name__ == '__main__':
-    app.run()
+  app.run(host='0.0.0.0', port=80)
