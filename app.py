75d3add928249c638c74d935a9e77004e901cafb
simonwoodworth <s.woodworth@ucc.ie>
2024-11-15 19:23:35 +0000

    Changed power function arguments

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    powerString = str(mymath.power(3,4))
+    powerString = str(mymath.power(3,3))
     return f'Hello World! The answer is: {powerString}'
 
 if __name__ == '__main__':
