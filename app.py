48af17bdb47a89f9c428be04490e1a3308058488
simonwoodworth <S.Woodworth@ucc.ie>
2024-11-18 09:53:56 +0000

    Update app.py to demonstrate editing in iuthub.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     powerString = str(mymath.power(3,8))
-    return f'Hello IS3313 on a REALLY HORRIBLE Monday morning. The answer is: {powerString}'
+    return f'Hello IS3313. This was edited in Github. The answer is: {powerString}'
 
 if __name__ == '__main__':
   app.run()
