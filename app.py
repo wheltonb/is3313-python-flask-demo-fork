ff88093ca1d0cc917511653e04db10b0de00d6df
simonwoodworth <s.woodworth@ucc.ie>
2024-11-18 09:50:59 +0000

    Updated to demonstrate pipeline.

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    powerString = str(mymath.power(2,8))
-    return f'Hello IS3313 on a wet Monday morning. The answer is: {powerString}'
+    powerString = str(mymath.power(3,8))
+    return f'Hello IS3313 on a REALLY HORRIBLE Monday morning. The answer is: {powerString}'
 
 if __name__ == '__main__':
   app.run()
